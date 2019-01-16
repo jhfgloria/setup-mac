bbd32adfb9262dc767bff095937e45cc3a91f0f2
João Glória <jhfgloria@gmail.com>
2019-01-16 22:51:09 +0000

    Update iterm-vscode-script.sh

--- iterm-vscode-script.sh
+++ iterm-vscode-script.sh
@@ -5,4 +5,16 @@ echo "\nZSH_THEME=\"af-magic\"" >> ~/.zshrc
 brew cask install visual-studio-code
 code --install-extension eamodio.gitlens
 code --install-extension equinusocio.vsc-material-theme
+code --install-extension kisstkondoros.typelens
+code --install-extension aliariff.auto-add-brackets
+code --install-extension coenraads.bracket-pair-colorizer-2
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension wix.vscode-import-cost
+code --install-extension formulahendry.auto-rename-tag
+brew tap caskroom/fonts
+brew cask install font-fira-code
+# Change font family on VSCode
+# "editor.fontFamily": “’Fira Code’”,
+# ”editor.fontLigatures": true, 
+
 brew cleanup
