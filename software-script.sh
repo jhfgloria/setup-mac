5059ef8d7a33596b1defcd42060cbdb6f808a7f0
João Glória <jhfgloria@gmail.com>
2019-09-19 21:53:43 +0100

    Update install script
    - Add Ruby environment
    - Add Rails
    - Add Shiftit

--- software-script.sh
+++ software-script.sh
@@ -1,7 +1,12 @@
+# Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
+
+# Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo "\nZSH_THEME=\"af-magic\"" >> ~/.zshrc
+
+# Visual Studio Code
 brew cask install visual-studio-code
 code --install-extension eamodio.gitlens
 code --install-extension equinusocio.vsc-material-theme
@@ -15,6 +20,20 @@ brew tap caskroom/fonts
 brew cask install font-fira-code
 # Change font family on VSCode
 # "editor.fontFamily": “’Fira Code’”,
-# ”editor.fontLigatures": true, 
+# ”editor.fontLigatures": true,
 
+# Shiftit
+brew cask install shiftit
+
+# Ruby runtime
+brew install rbenv ruby-build
+echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
+source ~/.bash_profile
+rbenv install 2.6.3
+rbenv global 2.6.3
+
+# Rails
+gem install rails
+
+# Cleanup
 brew cleanup
